eff366c31f5f13eec79358b21b6b2b2f26b5d620
Sven Anderson <sven@redhat.com>
2015-10-27 17:27:31 +0100

    install required devel packages for pip install

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # devstack-base
-Packer template for devstack base image
+Packer template for devstack base image based on fedora.
 
 It's automatically build by atlas on updates. Get it here: https://atlas.hashicorp.com/ansiwen/boxes/devstack-base
--- scripts/fedora/devstack-requirements.sh
+++ scripts/fedora/devstack-requirements.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -eux
 
 #dnf -y update
-dnf -y install rsyslog joe yum-utils net-tools nfs-utils mlocate telnet sudo git dnf python-pip libvirt
+dnf -y install rsyslog joe yum-utils net-tools nfs-utils mlocate telnet sudo git dnf python-pip
 
 pip install -U pip
 pip install -U setuptools
 
 git clone https://git.openstack.org/openstack-dev/devstack
 devstack/tools/install_prereqs.sh
-
 rm -rf devstack
 
 git clone https://git.openstack.org/openstack/requirements
-mkdir tmp
-pip install --cache-dir tmp -b tmp -U -r requirements/global-requirements.txt
-
-rm -rf requirements
-rm -rf tmp
+mkdir -p tmp build
+dnf -y install libvirt-devel sqlite-devel openldap-devel
+pip install --cache-dir tmp -b build --timeout 60 -U -r requirements/global-requirements.txt
+pip wheel --cache-dir tmp -b build --timeout 60 -r requirements/global-requirements.txt
+rm -rf requirements build tmp
+mv wheelhouse /usr/local

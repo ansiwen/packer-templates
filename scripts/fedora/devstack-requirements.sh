7e24f224f3cc3c9d4bba7b4f739b81f53981978a
Sven Anderson <sven@redhat.com>
2015-10-27 22:28:55 +0100

    move build and cache dir back to /tmp to save space

--- scripts/fedora/devstack-requirements.sh
+++ scripts/fedora/devstack-requirements.sh
@@ -5,15 +5,17 @@ dnf -y install rsyslog joe yum-utils net-tools nfs-utils mlocate telnet sudo git
 
 pip install -U pip
 pip install -U setuptools
+pip install -U wheel
 
-git clone https://git.openstack.org/openstack-dev/devstack
+git clone --depth=1 https://git.openstack.org/openstack-dev/devstack
 devstack/tools/install_prereqs.sh
 rm -rf devstack
 
-git clone https://git.openstack.org/openstack/requirements
-mkdir -p tmp build
+git clone --depth=1 https://git.openstack.org/openstack/requirements
 dnf -y install libvirt-devel sqlite-devel openldap-devel
-pip install --cache-dir tmp -b build --timeout 60 -U -r requirements/global-requirements.txt
-pip wheel --cache-dir tmp -b build --timeout 60 -r requirements/global-requirements.txt
-rm -rf requirements build tmp
+grep -v '^$\|#' requirements/global-requirements.txt | while read req ; do
+    pip wheel --no-cache-dir --timeout 60 "$req"
+    pip install --no-index --find-links wheelhouse --no-cache-dir -U "$req"
+done
+rm -rf requirements
 mv wheelhouse /usr/local

a428daeca85667248adaad74fcbd1c7891566749
Sven Anderson <sven@redhat.com>
2015-11-03 23:14:14 +0100

    move pip install before setup of pip.conf

--- scripts/devstack-requirements.sh
+++ scripts/devstack-requirements.sh
@@ -12,16 +12,16 @@ dnf -y install libvirt-devel sqlite-devel openldap-devel libjpeg-devel mysql rab
 
 git clone --depth=1 https://git.openstack.org/openstack/requirements
 
+pip install -U --no-cache-dir --timeout 60 pip
+pip install -U --no-cache-dir --timeout 60 setuptools
+pip install -U --no-cache-dir --timeout 60 wheel
+pip install -U --no-cache-dir --timeout 60 cffi
+
 cat >/etc/pip.conf <<EOF
 [global]
 wheel-dir = /var/wheelhouse
 find-links = /var/wheelhouse
 EOF
-
-pip install -U --no-cache-dir --timeout 60 pip
-pip install -U --no-cache-dir --timeout 60 setuptools
-pip install -U --no-cache-dir --timeout 60 wheel
-pip install -U --no-cache-dir --timeout 60 cffi
 
 grep -v '^$\|#' requirements/global-requirements.txt | while read req ; do
     pip wheel --no-cache-dir --timeout 60 $req

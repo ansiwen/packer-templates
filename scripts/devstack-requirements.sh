0b1487c287f959ae01a7d11adbeace67e02f785b
Sven Anderson <sven@redhat.com>
2015-11-04 00:06:53 +0100

    use /run/tmp

--- scripts/devstack-requirements.sh
+++ scripts/devstack-requirements.sh
@@ -2,7 +2,8 @@
 
 dnf -y install rsyslog joe yum-utils net-tools nfs-utils mlocate telnet sudo git dnf python-pip
 
-cd /tmp
+mkdir -p /run/tmp
+cd /run/tmp
 
 git clone --depth=1 https://git.openstack.org/openstack-dev/devstack
 devstack/tools/install_prereqs.sh
@@ -12,10 +13,10 @@ dnf -y install libvirt-devel sqlite-devel openldap-devel libjpeg-devel mysql rab
 
 git clone --depth=1 https://git.openstack.org/openstack/requirements
 
-pip install -U --no-cache-dir --timeout 60 pip
-pip install -U --no-cache-dir --timeout 60 setuptools
-pip install -U --no-cache-dir --timeout 60 wheel
-pip install -U --no-cache-dir --timeout 60 cffi
+pip install -U --no-cache-dir -b . --timeout 60 pip
+pip install -U --no-cache-dir -b . --timeout 60 setuptools
+pip install -U --no-cache-dir -b . --timeout 60 wheel
+pip install -U --no-cache-dir -b . --timeout 60 cffi
 
 cat >/etc/pip.conf <<EOF
 [global]
@@ -24,10 +25,10 @@ find-links = /var/wheelhouse
 EOF
 
 grep -v '^$\|#' requirements/global-requirements.txt | while read req ; do
-    pip wheel --no-cache-dir --timeout 60 $req
+    pip wheel --no-cache-dir -b . --timeout 60 $req
 done
 grep -v '^$\|#' requirements/upper-constraints.txt.txt | while read req ; do
-    pip wheel --no-cache-dir --timeout 60 $req
+    pip wheel --no-cache-dir -b . --timeout 60 $req
 done
 rm -rf requirements
 chown -R vagrant:vagrant /var/wheelhouse

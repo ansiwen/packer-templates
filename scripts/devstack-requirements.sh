f6603c0d4432e1fcf5e861d57b8f38e8bef6498c
Sven Anderson <sven@redhat.com>
2015-11-17 23:04:43 +0100

    add some more packages

--- scripts/devstack-requirements.sh
+++ scripts/devstack-requirements.sh
@@ -1,17 +1,10 @@
 #!/bin/sh -eux
 
-dnf -y install rsyslog joe yum-utils net-tools nfs-utils mlocate telnet sudo git dnf dnf-plugins-core python-pip
+dnf -y install rsyslog yum-utils net-tools nfs-utils mlocate telnet sudo \
+  git dnf dnf-plugins-core python-pip libvirt-devel sqlite-devel \
+  openldap-devel libjpeg-devel mariadb-server rabbitmq-server vim joe mod_wsgi \
+  scsi-target-utils kvm qemu-kvm libvirt libvirt-devel
 
-mkdir -p /run/tmp
-cd /run/tmp
-
-git clone --depth=1 https://git.openstack.org/openstack-dev/devstack
+git clone --depth=1 http://git.openstack.org/openstack-dev/devstack
 devstack/tools/install_prereqs.sh
 rm -rf devstack
-
-dnf -y install libvirt-devel sqlite-devel openldap-devel libjpeg-devel mysql rabbitmq-server
-
-pip install -U --timeout 60 pip
-pip install -U --timeout 60 setuptools
-pip install -U --timeout 60 wheel
-pip install -U --timeout 60 cffi
